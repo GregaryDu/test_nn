a2e361ba027c5a3cd52db5c0924614b0822ba207
yujianmin <jxyyjm@gmail.com>
2017-06-17 16:23:06 +0800

    bpnn

--- src/mine_nn.py
+++ src/mine_nn.py
@@ -51,10 +51,10 @@ class CMyNN:
 		x    = digits.data
 		y    = digits.target
 		train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.2, random_state=50)
-		self.train_x = train_x/255
+		self.train_x = train_x
 		#self.train_y = train_y
 		self.train_y = self.single2onehotmat(train_y)
-		self.test_x  = test_x/255
+		self.test_x  = test_x
 		#self.test_y  = test_y
 		self.test_y  = self.single2onehotmat(test_y)
 		print (train_x.shape)
@@ -66,14 +66,15 @@ class CMyNN:
 		return 1/(1+np.exp(-x))
 	def delt_h(self, x):
 		return -np.exp(-x)/((1+np.exp(-x))**2)
+		#return self.sigmoid(x) * (1-self.sigmoid(x))
 	def make_label_pred(self, pred_mat):
 		return np.argmax(pred_mat, axis=1)
 	def comp_mean_error(self, y, y_pred):
 		return np.mean(y_pred - y, axis = 1)
-	def compute_confusion(self, W_1, W_2, x, y):
-		Hid_input = np.dot(x, W_1)                  ## (m*64)*(64*100) --> (m*100)
+	def compute_confusion(self, W_1, W_2, x, y, b_1, b_2):
+		Hid_input = np.dot(x, W_1) + b_1            ## (m*64)*(64*100) --> (m*100)
 		Hid_output= self.sigmoid(Hid_input)         ## (m*100)         --> (m*100)
-		pred_input= np.dot(Hid_output, W_2)         ## (m*100)*(100*10)--> (m*10)
+		pred_input= np.dot(Hid_output, W_2) + b_2   ## (m*100)*(100*10)--> (m*10)
 		pred      = self.sigmoid(pred_input)        ## (m*10)          --> (m*10)
 		pred_label= self.make_label_pred(pred)
 		real_label= self.make_label_pred(y)
@@ -87,76 +88,41 @@ class CMyNN:
 		W_2 = np.reshape(np.array(np.random.normal(0, 0.003, 10*hidden_node_num)), (hidden_node_num, 10))
 		#W_1  = np.zeros((64, hidden_node_num))
 		#W_2  = np.zeros((hidden_node_num, 10))
+		b_1 = np.zeros((100,))
+		b_2 = np.zeros((10,))
 		delta_W_1 = np.zeros((64, hidden_node_num)) ## used to save the delta-of-w1
 		delta_W_2 = np.zeros((hidden_node_num, 10)) ## 
-		iter_num  = 50
-		alpha     = 0.03
+		iter_num  = 400
+		alpha     = 0.0005
 		for i in xrange(iter_num):
 			# 1) compute predict-y
-			Hid_input = np.dot(self.train_x, W_1)   ## (m*64)*(64*100) --> (m*100)
-			#print (Hid_input.shape)
-			Hid_output= self.sigmoid(Hid_input)          ## (m*100)         --> (m*100)
-			#print (Hid_output.shape)
-			pred_input= np.dot(Hid_output, W_2)     ## (m*100)*(100*10)--> (m*10)
-			#print (pred_input.shape)
-			pred      = self.sigmoid(pred_input)         ## (m*10)          --> (m*10)
-			print (pred[0:10, :])
+			Hid_input = np.dot(self.train_x, W_1) + b_1   ## (m*64)*(64*100) --> (m*100)
+			Hid_output= self.sigmoid(Hid_input)           ## (m*100)         --> (m*100)
+			Hid_output_delt = self.delt_h(Hid_output)
+			pred_input= np.dot(Hid_output, W_2) + b_2     ## (m*100)*(100*10)--> (m*10)
+			pred      = self.sigmoid(pred_input)          ## (m*10)          --> (m*10)
+			pred_delt = self.delt_h(pred)
+			#print (pred[0:10, :])#
 			# 1.1) compute label-pred confusion
 			print ('==== testing data ====')
-			self.compute_confusion(W_1, W_2, self.test_x, self.test_y)
+			self.compute_confusion(W_1, W_2, self.test_x, self.test_y, b_1, b_2)
 			print ('==== training data ====')
-			self.compute_confusion(W_1, W_2, self.train_x, self.train_y)
+			self.compute_confusion(W_1, W_2, self.train_x, self.train_y, b_1, b_2)
 			# 2) compute delta-each-layer
 			row       = self.train_y.shape[0]
 			#error     = np.mean(pred - self.train_y, 1)   ## (m*10)  --> (m*10)
-			error     = pred - self.train_y         ## (m*10)  --> (m*10)
-			print (error.shape)
-			delta_3   = error * self.delt_h(pred)   ## (m*10) .* (m*10)--> (m*10)
-			delta_W_2 = np.dot(Hid_output.T, delta_3) ## (m*100).T*(m*10) --> (100*10)
-			delta_2   = np.dot(delta_3, W_2.T) * self.delt_h(Hid_output)# --> (m*100)
-			delta_W_1 = np.dot(self.train_x.T, delta_2)## (64*m)*(m*100)  --> (64*100)
+			error_3   = pred - self.train_y         ## (m*10)  --> (m*10)
+			delta_3   = error_3 * pred_delt         ## (m*10) .* (m*10)--> (m*10)
+			delta_W_2 = np.dot(Hid_output.T, delta_3)/row   ## (m*100).T*(m*10) --> (100*10)
+			error_2   = np.dot(delta_3, W_2.T)      ## --> (m*100)
+			delta_2   = error_2 * Hid_output_delt   ## --> (m*100)
+			delta_W_1 = np.dot(self.train_x.T, delta_2)/row ## (64*m)*(m*100)   --> (64*100)
 			# 3) update  the par-w
-			W_1 = W_1 + alpha*delta_W_1/row
-			W_2 = W_2 + alpha*delta_W_2/row
+			W_2 = W_2 - alpha*delta_W_2
+			W_1 = W_1 - alpha*delta_W_1
+			b_2 = b_2 - alpha*np.sum(delta_3, axis=0)/row
+			b_1 = b_1 - alpha*np.sum(delta_2, axis=0)/row
 
-		pass
-	def my_nn_withNoMat(self):
-		hidden_node_num = 100
-		W_1 = np.reshape(np.array(np.random.normal(0, 0.003, 64*hidden_node_num)), (64, hidden_node_num))
-		W_2 = np.reshape(np.array(np.random.normal(0, 0.003, 10*hidden_node_num)), (hidden_node_num, 10))
-		#W_1  = np.zeros((64, hidden_node_num))
-		#W_2  = np.zeros((hidden_node_num, 10))
-		delta_W_1 = np.zeros((64, hidden_node_num)) ## used to save the delta-of-w1
-		delta_W_2 = np.zeros((hidden_node_num, 10)) ## 
-		iter_num  = 50
-		alpha     = 0.03
-		for i in xrange(iter_num):
-			# 1) compute predict-y
-			Hid_input = np.dot(self.train_x, W_1)   ## (m*64)*(64*100) --> (m*100)
-			#print (Hid_input.shape)
-			Hid_output= self.sigmoid(Hid_input)          ## (m*100)         --> (m*100)
-			#print (Hid_output.shape)
-			pred_input= np.dot(Hid_output, W_2)     ## (m*100)*(100*10)--> (m*10)
-			#print (pred_input.shape)
-			pred      = self.sigmoid(pred_input)         ## (m*10)          --> (m*10)
-			print (pred[0:10, :])
-			# 1.1) compute label-pred confusion
-			print ('==== testing data ====')
-			self.compute_confusion(W_1, W_2, self.test_x, self.test_y)
-			print ('==== training data ====')
-			self.compute_confusion(W_1, W_2, self.train_x, self.train_y)
-			# 2) compute delta-each-layer
-			row       = self.train_y.shape[0]
-			#error     = np.mean(pred - self.train_y, 1)   ## (m*10)  --> (m*10)
-			error     = pred - self.train_y         ## (m*10)  --> (m*10)
-			print (error.shape)
-			delta_3   = error * self.delt_h(pred)   ## (m*10) .* (m*10)--> (m*10)
-			delta_W_2 = np.dot(Hid_output.T, delta_3) ## (m*100).T*(m*10) --> (100*10)
-			delta_2   = np.dot(delta_3, W_2.T) * self.delt_h(Hid_output)# --> (m*100)
-			delta_W_1 = np.dot(self.train_x.T, delta_2)## (64*m)*(m*100)  --> (64*100)
-			# 3) update  the par-w
-			W_1 = W_1 + alpha*delta_W_1/row
-			W_2 = W_2 + alpha*delta_W_2/row
 
 		pass
 	def model_save(self):
