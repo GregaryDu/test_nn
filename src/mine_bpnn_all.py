3183cdd3dd8f34615ea4f9a70724ab17bdca626d
yujianmin <jxyyjm@gmail.com>
2017-07-05 23:20:51 +0800

    bpnn with batch_size training

--- src/mine_bpnn_all.py
+++ src/mine_bpnn_all.py
@@ -13,8 +13,19 @@ import logging
 import numpy as np
 from sklearn import metrics
 from sklearn import datasets
-tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.examples.tutorials.mnist import input_data
 
+## =========================================== ##
+'''
+== input-layer ==  |  == hidden-layer-1 == | == hidden-layer-2 == | == output-layer ==
+I[0]-W[0]B[0]-P[0] |  I[1]-W[1]B[1]-P[1]   |   I[2]-W[2]B[2]-P[2] |    I[3]-W[3]B[3]-P[3]
+E[0]-DH[0]-D[0]    |  E[1]-DH[1]-D[1]      |   E[2]-DH[2]-D[2]    |    E[3]-DH[3]-D[3]
+DW[0]-DB[0]        |  DW[1]-DB[1]          |   DW[2]-DB[2]        |    DW[3]-DB[3]
+'''
+#  I--input;     W--weight;               P--probabilty
+#  E--error;     DH-delt_active_function; D--delt
+#  DW--delt_W;   DB--delt_B
+## =========================================== ##
 
 class CMyNN:
 	def __init__(self, hidden_nodes_list=[10, 10], batch_size=100, epoch=100):
@@ -24,6 +35,11 @@ class CMyNN:
 		self.W          = []
 		self.B          = []
 		self.C          = []
+		self.middle_res = {}
+		self.epoch      = epoch
+		self.batch_size = batch_size
+		self.layer_num  = len(hidden_nodes_list)+2
+		self.hidden_nodes_list = hidden_nodes_list
 	def __del__(self):
 		self.train_data = ''
 		self.test_data  = ''
@@ -31,170 +47,131 @@ class CMyNN:
 		self.W          = []
 		self.B          = []
 		self.C          = []
+		self.middle_res = {}
+		self.epoch      = ''
+		self.batch_size = ''
+		self.layer_num  = ''
+		self.hidden_nodes_list = []
+
 	def read_data(self):
 		mnist = input_data.read_data_sets('./MNIST_data', one_hot=True)
 		self.train_data = mnist.train
 		self.test_data  = mnist.test
 	def sigmoid(self, x):
 		return 1/(1+np.exp(-x))
-	def delt_h(self, x):
+	def delt_sigmoid(self, x):
 		#return -np.exp(-x)/((1+np.exp(-x))**2)
 		return self.sigmoid(x) * (1-self.sigmoid(x))
-	def make_label_pred(self, pred_mat):
+	def get_label_pred(self, pred_mat):
 		return np.argmax(pred_mat, axis=1)
-	def initial_parameters(self):
+	def compute_layer_input(self, layer_num):
+		input = self.middle_res['layer_prob'][layer_num-1]
+		return np.dot(input, self.W[layer_num-1]) + self.B[layer_num-1]
+	def compute_layer_active(self, layer_num):
+		return self.sigmoid(self.middle_res['layer_input'][layer_num])
+	def compute_layer_delt(self, layer_num):
+		return self.delt_sigmoid(self.middle_res['layer_input'][layer_num])
+
+	def initial_weight_parameters(self):
 		input_node_num  = self.train_data.images.shape[1]
 		output_node_num = self.train_data.labels.shape[1]
-		h_layers_num    = len(self.hidden_nodes_list)
-		if h_layers_num<=0: print ('please input hidden_nodes_list, such as [10, 10]'); return ''
-		for i in xrange(h_layers_num + 1):
-			h_node_num = hidden_node_list[i]
-			h_inpu_num = hidden_node_list[i-1] if i>=1 else 0
-			if i == 0:
-				self.W[i] = np.reshape(np.array(np.random.normal(0, 0.001, input_node_num*h_node_num)), (input_node_num, h_node_num))
-				self.C[i] = np.array(np.random.normal(0, 0.001, input_node_num)) ## 反向时的 Bias ##
-				self.B[i] = np.array(np.random.normal(0, 0.001, h_node_num))     ## 正向时的 Bias ##
-			elif i == len(h_layers_num):
-				self.W[i] = np.reshape(np.array(np.random.normal(0, 0.001, h_node_num*output_node_num)),(h_node_num, output_node_num))
-				self.C[i] = np.array(np.random.normal(0, 0.001, h_node_num))
-				self.B[i] = np.array(np.random.normal(0, 0.001, output_node_num))
+		## 构造各层W (输入, 输出) node-num-pair 的 list ##
+		node_num_pair   = [(input_node_num, self.hidden_nodes_list[0])]
+		for i in xrange(len(self.hidden_nodes_list)):
+			if i == len(self.hidden_nodes_list)-1: node_num_pair.append((self.hidden_nodes_list[i], output_node_num))
+			else: node_num_pair.append((self.hidden_nodes_list[i], self.hidden_nodes_list[i+1]))
+		for node_num_i, node_num_o in node_num_pair:
+			W = np.reshape(np.array(np.random.normal(0, 0.001, node_num_i*node_num_o)), (node_num_i, node_num_o))
+			B = np.array(np.random.normal(0, 0.001, node_num_o))     ## 正向时的 Bias ##
+			self.W.append(W)
+			self.B.append(B)
+		## W,B,C len is self.layer_num-1 ##
+	def initial_middle_parameters(self):
+		self.middle_res['delt_W'] = [np.zeros_like(i) for i in self.W]  ## 层与层之间的连接权重       #
+		self.middle_res['delt_B'] = [np.zeros_like(i) for i in self.B]  ## 前向传播时的偏倚量         #
+		self.middle_res['layer_input'] = ['' for i in xrange(self.layer_num) ]   ## 前向传播时的每层输入       #
+		self.middle_res['layer_prob']  = ['' for i in xrange(self.layer_num) ]   ## 前向传播时的每层激活概率   #
+		self.middle_res['layer_delt']  = ['' for i in xrange(self.layer_num) ]   ## 每层误差回传时，的起始误差 #
+		self.middle_res['layer_wucha'] = ['' for i in xrange(self.layer_num) ]   ## 每层由后层传递过来的误差   #
+		self.middle_res['layer_active_delt'] = ['' for i in xrange(self.layer_num) ] ## 前向传播的激活后导数   #
+	def forward_propagate(self, batch_x):
+		## 前向依层，计算 --> output ## 并保存中间结果 ##
+		## 0-layer ====== 1-layer; ====== 2-layer; ===== output-layer ##
+		## I[0]-W[0]-B[0] I[1]-W[1]-B[1]  I[2]-W[2]-B[2] 
+		for layer_num in xrange(self.layer_num):
+			if layer_num == 0:
+				self.middle_res['layer_input'][layer_num] = batch_x
+				self.middle_res['layer_prob'][layer_num]  = batch_x
+				self.middle_res['layer_active_delt'][layer_num] = batch_x
 			else:
-				self.W[i] = np.reshape(np.array(np.random.normal(0, 0.001, h_inpu_num*h_node_num)), (h_inpu_num, h_node_num))
-				self.C[i] = np.array(np.random.normal(0, 0.001, h_inpu_num))
-				self.B[i] = np.array(np.random.normal(0, 0.001, h_node_num))
+				self.middle_res['layer_input'][layer_num] = self.compute_layer_input(layer_num)
+				self.middle_res['layer_prob'][layer_num]  = self.compute_layer_active(layer_num)
+				self.middle_res['layer_active_delt'][layer_num]= self.compute_layer_delt(layer_num)
+	def compute_output_prob(self, x):
+		for layer_num in xrange(self.layer_num):
+			if layer_num == 0:
+				output = x
+			else:
+				layer_num = layer_num -1
+				output = self.sigmoid(np.dot(output, self.W[layer_num])+self.B[layer_num])
+		return output
+	def backward_propagate(self, batch_y):
+		## 后向依层，计算 --> delt ## 并保存中间结果 ##
+		for layer_num in xrange(self.layer_num, 0, -1):
+			layer_num = layer_num - 1
+			if layer_num == (self.layer_num -1):
+				self.middle_res['layer_wucha'][layer_num] = self.middle_res['layer_prob'][layer_num] - batch_y
+				self.middle_res['layer_delt'][layer_num]  = \
+					self.middle_res['layer_wucha'][layer_num] * self.middle_res['layer_active_delt'][layer_num]
+			else:
+				self.middle_res['layer_wucha'][layer_num] = \
+					np.dot(self.middle_res['layer_delt'][layer_num+1], self.W[layer_num].T)
+				self.middle_res['layer_delt'][layer_num]  = \
+					self.middle_res['layer_wucha'][layer_num] * self.middle_res['layer_active_delt'][layer_num]
+				self.middle_res['delt_W'][layer_num]      = \
+					np.dot(self.middle_res['layer_prob'][layer_num].T, self.middle_res['layer_delt'][layer_num+1])/self.batch_size
+				self.middle_res['delt_B'][layer_num]      = np.mean(self.middle_res['layer_delt'][layer_num+1], axis=0)
+				
+	def update_weight(self):
+		# for convient, compute from input to output dir #
+		for layer_num in xrange(self.layer_num-1):
+			#print ('layer_num:\t', layer_num, '\tself.W.shape:\t', self.W[layer_num].shape, \
+			#		'\tself.middle_res[delt_W].shape:\t', self.middle_res['delt_W'][layer_num].shape)
+			#print ('layer_num:\t', layer_num, '\tself.B.shape:\t', self.B[layer_num].shape, \
+			#		'\tself.middle_res[delt_B].shape:\t', self.middle_res['delt_B'][layer_num].shape)
+			self.W[layer_num] -= self.middle_res['delt_W'][layer_num]
+			self.B[layer_num] -= self.middle_res['delt_B'][layer_num]
+
 	def my_nn(self):
-		self.initial_parameters()
-		middle_res = {}
-		middle_res['delta_W'] = [np.zeros_like(i) for i in self.W]
-		middle_res['delta_B'] = [np.zeros_like(i) for i in self.B]
-		middle_res['delta_C'] = [np.zeros_like(i) for i in self.C]
-		middle_res['layer_input'] = ['' for i in ]
-		middle_res['layer_prob']  =
 
+		self.read_data()
+		self.initial_weight_parameters()
+		self.initial_middle_parameters()
 		iter_num= self.epoch*int(self.train_data.images.shape[0]/self.batch_size)
-		for i in xrange(iter_num):
-			batch_data = self.train_data.next_batch(self.batch_size)
-			batch_x    = batch_data.images
-			batch_y    = batch_data.labels
-			# 1) compute predict-y
-			Hid_input = np.dot(self.train_x, W_1) + B_1   ## (m*64)*(64*100) --> (m*100)
-			Hid_output= self.sigmoid(Hid_input)           ## (m*100)         --> (m*100)
-			#print ('Hid_output.shape', Hid_output.shape)
-			Hid_output_delt = self.delt_h(Hid_output)
-			pred_input= np.dot(Hid_output, W_2) + B_2     ## (m*100)*(100*10)--> (m*10)
-			pred      = self.sigmoid(pred_input)          ## (m*10)          --> (m*10)
-			#print ('pred.shape', pred.shape)
-			pred_delt = self.delt_h(pred)
-			#print (pred[0:10, :])#
-			# 1.1) compute label-pred confusion
-			if iter_num%100 == 0:
-				#print ('==== testing data ====')
-				#self.compute_confusion(W_1, W_2, self.test_x, self.test_y, B_1, B_2)
-				#print ('==== training data ====')
-				#self.compute_confusion(W_1, W_2, self.train_x, self.train_y, B_1, B_2)
-				H2 = self.sigmoid(np.dot(self.test_x, W_1)+B_1)
-				P  = self.sigmoid(np.dot(H2, W_2)+B_2)
-				PL = self.make_label_pred(P)
-				RL = self.make_label_pred(self.test_y)
-				print ('iter_num :', i, \
-						'train mean error', np.mean(pred - self.train_y), \
-						'pred mean error',np.mean(P - self.test_y), \
-						'pred accuracy', metrics.accuracy_score(RL, PL))
-			# 2) compute delta-each-layer
-			row       = self.train_y.shape[0]
-			
-			error_3   = pred - self.train_y         ## (m*10)  --> (m*10)
-			delta_3   = error_3 * pred_delt         ## (m*10) .* (m*10)--> (m*10)
-			delta_W_2 = np.dot(Hid_output.T, delta_3)   ## (m*100).T*(m*10) --> (100*10)
-			
-			error_2   = np.dot(delta_3, W_2.T)      ## --> (m*100)
-			delta_2   = error_2 * Hid_output_delt   ## --> (m*100)
-			delta_W_1 = np.dot(self.train_x.T, delta_2) ## (64*m)*(m*100)   --> (64*100)
-			# 3) update  the par-w
-			W_2 = W_2 - alpha * delta_W_2/row
-			W_1 = W_1 - alpha * delta_W_1/row
-			B_2 = B_2 - alpha * np.sum(delta_3, axis=0)/row
-			B_1 = B_1 - alpha * np.sum(delta_2, axis=0)/row
-#			if iter_num%5000==0:
-#				print (W_2)
-#				print (W_1)
-#				print (B_2)
-#				print (B_1)
-	def my_nn_withMoment(self, iter_num, alpha, hidden_num):
-		# notice: this is not a standand-moment method, just a native-moment #
-		hidden_node_num = hidden_num
-		input_node_num  = self.train_x.shape[1]
-		output_node_num = self.train_y.shape[1]
-		iter_num        = iter_num
-		alpha           = alpha
-
-		W_1 = np.reshape(np.array(np.random.normal(0, 0.001, 64*hidden_node_num)), (64, hidden_node_num))
-		W_2 = np.reshape(np.array(np.random.normal(0, 0.001, 10*hidden_node_num)), (hidden_node_num, 10))
-		B_1 = np.array(np.random.normal(0, 0.001, hidden_node_num))
-		B_2 = np.array(np.random.normal(0, 0.001,  output_node_num))
-		
-		delta_W_1 = np.zeros((input_node_num, hidden_node_num)) ## used to save the delta-of-w1
-		delta_W_2 = np.zeros((hidden_node_num, output_node_num)) ## 
-		delta_W_1_pre = np.zeros((input_node_num, hidden_node_num)) ## used to save the delta-of-w1 before current
-		delta_W_2_pre = np.zeros((hidden_node_num, output_node_num)) ## 
-		sample_num    = self.train_y.shape[0]
-		delta_3_pre   = np.zeros((sample_num, output_node_num))
-		delta_2_pre   = np.zeros((sample_num, hidden_node_num))
-		pre_alpha     = 0.8
-		cur_alpha     = 1 - pre_alpha
 
 		for i in xrange(iter_num):
+			batch_x, batch_y = self.train_data.next_batch(self.batch_size)
 			# 1) compute predict-y
-			Hid_input = np.dot(self.train_x, W_1) + B_1   ## (m*64)*(64*100) --> (m*100)
-			Hid_output= self.sigmoid(Hid_input)           ## (m*100)         --> (m*100)
-			Hid_output_delt = self.delt_h(Hid_output)
-			pred_input= np.dot(Hid_output, W_2) + B_2     ## (m*100)*(100*10)--> (m*10)
-			pred      = self.sigmoid(pred_input)          ## (m*10)          --> (m*10)
-			pred_delt = self.delt_h(pred)
-			# 1.1) compute label-pred confusion
-			if iter_num%100 == 0:
-				H2 = self.sigmoid(np.dot(self.test_x, W_1)+B_1)
-				P  = self.sigmoid(np.dot(H2, W_2)+B_2)
-				PL = self.make_label_pred(P)
-				RL = self.make_label_pred(self.test_y)
-				print ('iter_num :', i, \
-						'train mean error', np.mean(pred - self.train_y), \
-						'pred mean error',np.mean(P - self.test_y), \
-						'pred accuracy', metrics.accuracy_score(RL, PL))
+			self.forward_propagate(batch_x)
 			# 2) compute delta-each-layer
-			row       = self.train_y.shape[0]
-			
-			error_3   = pred - self.train_y         ## (m*10)  --> (m*10)
-			delta_3   = error_3 * pred_delt         ## (m*10) .* (m*10)--> (m*10)
-			delta_W_2 = np.dot(Hid_output.T, delta_3)   ## (m*100).T*(m*10) --> (100*10)
-			delta_W_2_pre = pre_alpha*delta_W_2_pre + cur_alpha*delta_W_2
-			delta_3_pre   = pre_alpha*delta_3_pre + cur_alpha*delta_3
+			self.backward_propagate(batch_y)
+			# 3) update  the par-w-B
+			self.update_weight()
+			# 4) training is doing... 
+			if i%100 == 0:
+				batch_x_prob = self.middle_res['layer_prob'][self.layer_num-1]
+				test_x_prob  = self.compute_output_prob(self.test_data.images)
+				batch_acc, batch_confMat = self.compute_accuracy_confusionMat(batch_y, batch_x_prob)
+				test_acc,  test_confMat  = self.compute_accuracy_confusionMat(self.test_data.labels, test_x_prob)
+				print ('epoch:', int(i/self.batch_size), 'iter:', i, 'train_batch_accuracy:', batch_acc, 'test_accuracy', test_acc)
+	
+	def compute_accuracy_confusionMat(self, real_mat, pred_mat):
+		pred_label= self.get_label_pred(pred_mat)
+		real_label= self.get_label_pred(real_mat)
+		accuracy  = metrics.accuracy_score(real_label, pred_label)
+		confu_mat = metrics.confusion_matrix(real_label, pred_label, np.unique(real_label))
+		return accuracy, confu_mat
 
-			error_2   = np.dot(delta_3, W_2.T)      ## --> (m*100)
-			delta_2   = error_2 * Hid_output_delt   ## --> (m*100)
-			delta_W_1 = np.dot(self.train_x.T, delta_2) ## (64*m)*(m*100)   --> (64*100)
-			delta_W_1_pre = pre_alpha*delta_W_1_pre + cur_alpha*delta_W_1
-			delta_2_pre   = pre_alpha*delta_2_pre + cur_alpha*delta_2
-
-			# 3) update  the par-w
-			W_2 = W_2 - alpha * delta_W_2_pre/row
-			W_1 = W_1 - alpha * delta_W_1_pre/row
-			B_2 = B_2 - alpha * np.sum(delta_3_pre, axis=0)/row
-			B_1 = B_1 - alpha * np.sum(delta_2_pre, axis=0)/row
-	def compute_confusion(self, W_1, W_2, x, y, B_1, B_2):
-		Hid_input = np.dot(x, W_1) + B_1            ## (m*64)*(64*100) --> (m*100)
-		Hid_output= self.sigmoid(Hid_input)         ## (m*100)         --> (m*100)
-		pred_input= np.dot(Hid_output, W_2) + B_2   ## (m*100)*(100*10)--> (m*10)
-		pred      = self.sigmoid(pred_input)        ## (m*10)          --> (m*10)
-		pred_label= self.make_label_pred(pred)
-		real_label= self.make_label_pred(y)
-		print ('accuracy : ', metrics.accuracy_score(real_label, pred_label))
-		print ('confusion matrix :')
-		print (metrics.confusion_matrix(real_label, pred_label, np.unique(real_label)))
-		print ('train_y:', self.train_y)
-		print ('pred_y :', pred_label)
 if __name__=='__main__':
-	CTest = CMyNN()
-	CTest.read_data_split()
-	CTest.my_nn(1000, 10, 100)
-	CTest.my_nn_withMoment(1000, 10, 100)
+	CTest = CMyNN(hidden_nodes_list=[100], batch_size=100, epoch=100)
+	CTest.my_nn()
